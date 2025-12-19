6c0dae34891daa147a45e98f3ee25800ffe6a15a
Prathiksha73 <kushiprathikshay@gmail.com>
2025-12-19 12:24:35 +0530

    changed view

--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,10 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 class TestProtectedAPI(APIView):
+    authentication_classes = [JWTAuthentication]   # 👈 explicitly JWT
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
@@ -10,4 +12,3 @@ class TestProtectedAPI(APIView):
             "message": "JWT authentication successful",
             "user": request.user.username
         })
-#CREATE YOUR VIEW HERE
